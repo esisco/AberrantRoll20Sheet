1b3632d1f96c9bf9f2f25c2c6c8280ba2860b26d
Eric Sisco <eric.sisco@gmail.com>
2026-01-21 13:14:46 -0600

    Add css

---
+++ generate_css.sh
@@ -0,0 +1,5 @@
+# This script stitches together all of the css files into a single file.
+
+cat ./css/main.css > ./Dist/AberrantRoll20Sheet.css
+cat ./css/rolltemplates.css >> ./Dist/AberrantRoll20Sheet.css
+cat ./css/tabs.css >> ./Dist/AberrantRoll20Sheet.css
